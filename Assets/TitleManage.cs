b2159a6cccf269ddedc1d1d6d20de38913232281
Shun NAKAYAMA <e245749@ie.u-ryukyu.ac.jp>
2026-06-30 10:57:41 +0900

    プレイ終了時にすぐに他の画面に遷移することの修正と遊び方追加

--- Assets/TitleManage.cs
+++ Assets/TitleManage.cs
@@ -6,12 +6,28 @@ public class TitleManager : MonoBehaviour
     [Header("切り替えるUI")]
     public GameObject titleLayout;      // 今のStartボタンが入っているグループ
     public GameObject stageSelectPanel; // さっき作ったステージ選択パネル
+    public GameObject howToPlayPanel;   // 遊び方を表示するパネル
 
     // 1. Startボタンが押されたとき
     public void ShowStageSelect()
     {
         titleLayout.SetActive(false);      // タイトルとStartボタンを隠す
         stageSelectPanel.SetActive(true); // ステージ選択を出す
+    }
+
+    // 遊び方ボタンが押されたとき：遊び方パネルを表示してタイトルを隠す
+    public void ShowHowToPlay()
+    {
+        if (titleLayout != null) titleLayout.SetActive(false);
+        if (howToPlayPanel != null) howToPlayPanel.SetActive(true);
+    }
+
+    // 遊び方パネルの「戻る」ボタンが押されたとき：タイトルに戻る
+    public void BackToTitle()
+    {
+        if (howToPlayPanel != null) howToPlayPanel.SetActive(false);
+        if (stageSelectPanel != null) stageSelectPanel.SetActive(false);
+        if (titleLayout != null) titleLayout.SetActive(true);
     }
 
     // 2. Stage 1が押されたとき
